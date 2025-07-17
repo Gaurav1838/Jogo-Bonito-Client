9d7b9e63dbc28ed71eb14585c9a8c3c1cfbf45c8
gaurav18 <tgaurav1213@gmail.com>
2025-07-17 20:12:02 +0530

    navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,10 +2,18 @@ import { Link } from 'react-router-dom';
 import './navbar.css';
 
 function Navbar() {
+  const handleNavLinkClick = () => {
+    const navbarCollapse = document.getElementById('navbarNav');
+    if (navbarCollapse && navbarCollapse.classList.contains('show')) {
+      const toggler = document.querySelector('.navbar-toggler');
+      if (toggler) toggler.click();
+    }
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top full-width-navbar">
       <div className="container-fluid px-4">
-        <Link className="navbar-brand d-flex align-items-center" to="/">
+        <Link className="navbar-brand d-flex align-items-center" to="/" onClick={handleNavLinkClick}>
           <img src="/logo.png" alt="JOGO BONITO" height="40" className="me-2" />
           <span className="fw-bold">JOGO BONITO</span>
         </Link>
@@ -21,16 +29,16 @@ function Navbar() {
         <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
+              <Link className="nav-link" to="/" onClick={handleNavLinkClick}>Home</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/shop">Shop</Link>
+              <Link className="nav-link" to="/shop" onClick={handleNavLinkClick}>Shop</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/cart">Cart</Link>
+              <Link className="nav-link" to="/cart" onClick={handleNavLinkClick}>Cart</Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact</Link>
+              <Link className="nav-link" to="/contact" onClick={handleNavLinkClick}>Contact</Link>
             </li>
           </ul>
         </div>
